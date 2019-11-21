7bdb27d6f61a46e9a7861678d384f9dbdb884556
rodrigodonizettio <rodrigodonizettio@gmail.com>
2019-11-21 00:25:43 -0300

    Mapping Audit Service for the framework

--- product-web/src/main/java/br/inatel/dm110/rest/RestApplication.java
+++ product-web/src/main/java/br/inatel/dm110/rest/RestApplication.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import br.inatel.dm110.impl.AuditService_Impl;
 import br.inatel.dm110.impl.ProductService_Impl;
 
 @ApplicationPath("/api")
@@ -14,6 +15,7 @@ public class RestApplication extends Application {
 	public Set<Class<?>> getClasses() {
 		Set<Class<?>> classes = new HashSet<>();
 		classes.add(ProductService_Impl.class);
+		classes.add(AuditService_Impl.class);
 		return classes;
 	}
 }
